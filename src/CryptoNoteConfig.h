84b3ac141c232756751e62084c36efa5ab59aa75
octavesism <39882927+octavesism@users.noreply.github.com>
2018-06-27 01:50:21 -0400

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -82,9 +82,9 @@ const size_t   BLOCKS_SYNCHRONIZING_DEFAULT_COUNT            =  200;    //by def
 const size_t   COMMAND_RPC_GET_BLOCKS_FAST_MAX_COUNT         =  1000;
 
 //TODO This port will be used by the daemon to establish connections with p2p network
-const int P2P_DEFAULT_PORT = 18851;
+const uint64_t P2P_DEFAULT_PORT = 18851;
 //TODO This port will be used by the daemon to interact with simlewallet
-const int RPC_DEFAULT_PORT = 18852;
+const uint64_t RPC_DEFAULT_PORT = 18852;
 
 const size_t   P2P_LOCAL_WHITE_PEERLIST_LIMIT                =  1000;
 const size_t   P2P_LOCAL_GRAY_PEERLIST_LIMIT                 =  5000;
